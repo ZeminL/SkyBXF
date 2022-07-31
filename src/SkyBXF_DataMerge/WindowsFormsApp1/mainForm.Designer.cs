e4c50d1fc28b2540e68c28c4e721abfc48d39880
psyzemin <psyzemin@gmail.com>
2022-07-31 15:48:51 +0800

    Add DataMerge source code

---
+++ src/SkyBXF_DataMerge/WindowsFormsApp1/mainForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(68, 57);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(187, 246);
+            this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(621, 364);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "导出";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(180, 364);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 31);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "全选";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(68, 363);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 32);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "反选";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(322, 57);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(374, 263);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Participant information";
+            this.columnHeader1.Width = 180;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Value";
+            this.columnHeader2.Width = 100;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "Form1";
+            this.Text = "Data Merge";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ src/SkyBXF_DataMerge/WindowsFormsApp1/mainForm.cs
@@ -0,0 +1,186 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string curDir;
+        private List<Participant> ParInfo = new List<Participant>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {            
+                curDir = Directory.GetCurrentDirectory() + "\\Result";
+                string[] dirs = Directory.GetDirectories(curDir, "Participant_*");            
+                foreach (var i in dirs)
+                {
+                    checkedListBox1.Items.Add(i.Substring(curDir.Length + 1));
+                    Participant p = JsonConvert.DeserializeObject<Participant>(File.ReadAllText(i + "\\ParticipantInfo.json"));
+                    ParInfo.Add(p);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            Participant p = ParInfo[checkedListBox1.SelectedIndex];
+            ListViewItem liv0 = new ListViewItem();
+            liv0.Text = "ID";
+            liv0.SubItems.Add(p.ID.ToString());
+            listView1.Items.Add(liv0);
+            ListViewItem liv1 = new ListViewItem();
+            liv1.Text = "Session";
+            liv1.SubItems.Add(p.Session.ToString());
+            listView1.Items.Add(liv1);
+            ListViewItem liv2 = new ListViewItem();
+            liv2.Text = "Block";
+            liv2.SubItems.Add(p.Block.ToString());
+            listView1.Items.Add(liv2);
+            ListViewItem liv3 = new ListViewItem();
+            liv3.Text = "Trial";
+            liv3.SubItems.Add(p.Trial.ToString());
+            listView1.Items.Add(liv3);
+            ListViewItem liv4 = new ListViewItem();
+            liv4.Text = "Gender";
+            liv4.SubItems.Add(p.Gender.ToString());
+            listView1.Items.Add(liv4);
+            ListViewItem liv5 = new ListViewItem();
+            liv5.Text = "Handedness";
+            liv5.SubItems.Add(p.Handedness.ToString());
+            listView1.Items.Add(liv5);
+        }
+
+        private List<string[]> DataRead(Participant p, string path)
+        {
+            string[] files = Directory.GetFiles(path, "result_" + p.ID + "*.json");
+            List<string[]> datas = new List<string[]>();
+            for (int i = 0; i < files.Length; i++)
+            {
+                FileInfo fileInfo = new FileInfo(files[i]);
+                var data = JsonConvert.DeserializeObject<Result_Data>(File.ReadAllText(fileInfo.FullName)).data;
+                if (i == 0)
+                {
+                    datas.Add(data);
+                }
+                else
+                {
+                    string[] d = new string[data.Length - 1];
+                    for (int j = 1; j < data.Length; j++)
+                    {
+                        d[j - 1] = data[j];
+                    }
+                    datas.Add(d);
+                }
+            }
+            return datas;
+        }
+
+        private void DataWrite(Participant p, List<string[]> datas)
+        {
+            string fullPath = curDir + "\\Data_CSV\\Data_" + p.ID + ".csv";
+            FileInfo CSVfile = new FileInfo(fullPath);
+            if (!CSVfile.Directory.Exists)
+            {
+                CSVfile.Directory.Create();
+            }
+            FileStream fs = new FileStream(fullPath, System.IO.FileMode.Create, System.IO.FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs, System.Text.Encoding.UTF8);
+            foreach (var item in datas)
+            {
+                foreach (var i in item)
+                {
+                    sw.WriteLine(i);
+                }
+            }
+
+            sw.Close();
+            fs.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var r = new List<int>();
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                if (checkedListBox1.GetItemChecked(i))
+                {
+                    string s = curDir + "\\" + checkedListBox1.Items[i].ToString();
+                    var list = DataRead((Participant)ParInfo[i], s);
+                    DataWrite((Participant)ParInfo[i], list);
+                    r.Add(ParInfo[i].ID);
+                }
+            }
+            string result = "";
+            int n = 0;
+            foreach (var item in r)
+            {
+                n++;
+                result += item;
+                if (n != r.Count)
+                {
+                    result += ", ";
+                }
+            }
+            MessageBox.Show("The data of the following participants has been successfully merged and exported: \n" + result,"");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                if (checkedListBox1.GetItemChecked(i))
+                {
+                    checkedListBox1.SetItemChecked(i, false);
+                }
+                else
+                {
+                    checkedListBox1.SetItemChecked(i, true);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, true);
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Participant
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Handedness { get; set; }
+        public int Session { get; set; }
+        public int Block { get; set; }
+        public int Trial { get; set; }
+    }
+
+    public class Result_Data
+    {
+        public string[] data { get; set; }
+    }
+}
